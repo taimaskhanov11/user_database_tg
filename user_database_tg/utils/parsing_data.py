756225647906fa928d7ab381d242c3ebf92894e5
taimaskhanov11 <81189712+taimaskhanov11@users.noreply.github.com>
2022-02-24 01:24:15 +0300

    update parce_datafiles

--- user_database_tg/utils/parsing_data.py
+++ user_database_tg/utils/parsing_data.py
@@ -95,7 +95,7 @@ def parce_datafiles(path: Path) -> list[tuple[str, str]]:
                     parce_data(f)
                     # continue
             except UnicodeDecodeError as e:
-                logger.critical(e)
+                logger.critical(f"{e}| UTF8")
                 with open(data_file, encoding="cp1251") as f:  # todo 2/24/2022 12:40 AM taima:
                     # print(f.readlines())
                     parce_data(f)
