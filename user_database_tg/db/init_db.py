77d68993f27475ba1296dec5c6b099ae7bbfd51e
taimaskhanov11 <81189712+taimaskhanov11@users.noreply.github.com>
2022-02-24 19:00:44 +0300

    update parce_datafiles

--- user_database_tg/db/init_db.py
+++ user_database_tg/db/init_db.py
@@ -1,4 +1,6 @@
 import sys
+import time
+import unicodedata
 from pathlib import Path
 from pprint import pprint
 
@@ -46,27 +48,66 @@ async def create_users():
     # await init_tortoise()
     # await init_tortoise(host="localhost", password="postgres")
     await init_tortoise(host="localhost")
+    batch_size = 500000
+
+    async def bulk_users_create(objs):
+        try:
+            await HackedUser.bulk_create(
+                objs,
+                batch_size=batch_size,
+            )
+        except Exception as e:
+            with open("incorrect_data/trash.txt", "w", encoding="utf-8") as f:
+                logger.debug("Запись в файл")
+                f.writelines(map(lambda x: f"{x[0]}:{x[1]}\n", users_data[pre:index]))
+            raise e
+
+    # for path in Path("../test2/").iterdir():
+    # for path in Path("../test_users_datafiles/").iterdir():
     for path in Path("/var/lib/postgresql/TO_IMPORT").iterdir():
-        # for path in Path("../users_datafiles/").iterdir():
+    # for path in Path("../users_datafiles/").iterdir():
         service = path.name
 
         logger.debug(f"Парс {service}...")
+        t = time.monotonic()
         users_data = parce_datafiles(path)
-        logger.debug(f"Запарсено данных {len(users_data)}")
+        t2 = time.monotonic() - t
+        logger.debug(f"Запарсено данных {len(users_data)}. {t2} s")
         # for data in users_data:
         # pprint(users_data)
-        users_obj = [HackedUser(email=x[0], password=x[1], service=service) for x in users_data]
 
+        # for a, b in users_data:
+        #     if a == "almacenestitch@hotmail.com" and b.startswith("ì"):
+        #         print(a, b)
+        #         print(b.replace("\x00", " "))
+        # exit()
+
+        pre = 0
+        for index in range(0, len(users_data), batch_size):
+            # for index in range(0, len(users_data), 10):
+            # print(pre, users_data)
+            logger.debug(f"Создание объектов {pre}:{index}")
+            users_objs = (HackedUser(email=x[0], password=x[1], service=service) for x in users_data[pre:index])
+            await bulk_users_create(users_objs)
+            logger.debug(f"Объекты созданы {pre}:{index}")
+            pre = index
+        logger.debug(f"Создание оставшихся {pre}: {len(users_data)}")
+        users_objs = (
+            HackedUser(email=x[0], password=x[1], service=service) for x in users_data[pre: len(users_data)]
+        )
+        await bulk_users_create(users_objs)
+        logger.debug(f"Созданы оставшиеся {pre}:{len(users_data)}")
+
+        # users_obj = [HackedUser(email=x[0], password=x[1], service=service) for x in users_data]
         # for user in users_obj:
         #     logger.trace(f"{user.email}{user.password}")
         #     await user.save()
-        # HackedUser.save()
 
-        await HackedUser.bulk_create(
-            users_obj,
-            batch_size=100000,
-
-        )
+        # await HackedUser.bulk_create(
+        #     users_obj,
+        #     batch_size=100000,
+        #
+        # )
         logger.info(f"{service}| Все данные сохранены")
 
 
--- user_database_tg/utils/parsing_data.py
+++ user_database_tg/utils/parsing_data.py
@@ -76,36 +76,50 @@ def parce_datafiles(path: Path) -> list[tuple[str, str]]:
     users_data = []
 
     def parce_user(data):
-        data = data.strip()
-        if data:
-            return re.findall(r"(.*):(.*)", data)[0]
-        else:
-            return "null", "null"
+        # data = data.strip() #todo 2/24/2022 6:38 PM taima:
+        # if data == "\n":
+        #     print(data)
+        # return re.findall(r"(.*):(.*)", data)[0] or "null", "null"
+        # logger.trace(data)
+        # if data:
+        #     login, password = re.findall(r"(.*):(.*)", data)[0]
+        #     return login.replace("\x00", " "), password.replace("\x00", " ")
+        # else:
+        #     logger.critical(data)
+        #     return "null", "null"
+        # if data:
+        login, password = re.findall(r"(.*):(.*)", data)[0]
+        return login.replace("\x00", " "), password.replace("\x00", " ")
+        # else:
+        #     logger.critical(data)
+        #     return "null", "null"
 
     def parce_data(f):
-        try:
-            data = tuple(
-                # map(lambda x: re.findall(r"(.*):(.*)", x.strip())[0] if x.strip() and "0x00" not in x else (
-                map(parce_user, f.readlines())) #todo 2/24/2022 2:00 AM taima: filter
-            # users_da
-            users_data.extend(list(data))
-        except Exception as e:
-            logger.critical(f"{path.name}|{data_file.name}")
-            # logger.critical(e)
-            raise e
-            # raise e
+        # print(f.readlines())
+        data = list(
+            # map(lambda x: re.findall(r"(.*):(.*)", x.strip())[0] if x.strip() and "0x00" not in x else (
+            # map(lambda x: re.findall(r"(.*):(.*)", x.strip())[0] if x.strip() else ("null", "null"), f.readlines()))
+            map(parce_user, f.readlines()))  # todo 2/24/2022 2:00 AM taima: filter
+        # map(lambda x: re.findall(r"(.*):(.*)", x)[0], f.readlines()))  # todo 2/24/2022 2:00 AM taima: filter
+        # map(lambda x: re.findall(r"(.*):(.*)", x.decode("utf-8"))[0], f.readlines()))  # todo 2/24/2022 2:00 AM taima: filter
+        # users_da
+        logger.debug(f"Получено {data_file.name}|[{len(data)}]")
+        users_data.extend(data)
 
     for data_file in path.iterdir():
+        # for data_file in [Path(r"C:\Users\taima\PycharmProjects\user_database_tg\user_database_tg\db\trash.txt")]:
         if data_file.name != "err_file.dd":
             logger.trace(f"Парс файла {data_file.name}")
             try:
                 with open(data_file, encoding="utf-8") as f:  # todo 2/24/2022 12:40 AM taima:
+                    # with open(data_file, mode="rb") as f:  # todo 2/24/2022 12:40 AM taima:
                     # print(f.readlines())
+                    # print(f)
                     parce_data(f)
                     # continue
             except UnicodeDecodeError as e:
-                logger.critical(f"{e}| UTF8")
-                continue  #todo 2/24/2022 3:46 PM taima:
+                logger.critical(f"{e}| UTF8|{path.name}|{data_file.name}")
+                # continue  # todo 2/24/2022 3:46 PM taima:
                 logger.warning(f"Повторный парс файла {path.name}|{data_file.name}")
                 with open(data_file, encoding="cp1251") as f:  # todo 2/24/2022 12:40 AM taima:
                     # print(f.readlines())
@@ -115,7 +129,7 @@ def parce_datafiles(path: Path) -> list[tuple[str, str]]:
 
 
 if __name__ == '__main__':
-    users_data = parce_datafiles(Path("../users_datafiles/webhost"))
+    users_data = parce_datafiles(Path("../users_datafiles/000webhost.com_(2015.03)"))
     pprint(users_data)
     # for data in users_data):
     #     print(data[0], data[1])
