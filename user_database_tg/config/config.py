fa0ab16c0b71df7363531e0bbe72bcae0b7fbf94
taimaskhanov11 <81189712+taimaskhanov11@users.noreply.github.com>
2022-02-26 12:44:32 +0300

    update toml

--- user_database_tg/config/config.py
+++ user_database_tg/config/config.py
@@ -1,10 +1,15 @@
-from environs import Env
+# from environs import Env
+import os
+from distutils.util import strtobool
+
 from pyqiwip2p.AioQiwip2p import AioQiwiP2P
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
+# env = Env()
+# env.read_env()
+config = dotenv_values(".env")
 
-env = Env()
-env.read_env()
-
-TG_TOKEN = env.str("TG_TOKEN")
-QIWI_TOKEN = env.str("QIWI_TOKEN")
-TEST = env.bool("TEST")
+TG_TOKEN = os.getenv("TG_TOKEN")
+QIWI_TOKEN = os.getenv("QIWI_TOKEN")
+TEST = strtobool(os.getenv("TEST"))
 p2p = AioQiwiP2P(auth_key=QIWI_TOKEN)
