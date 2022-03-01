8c00c76cb3cf19898648785d345572a59ae32802
taimaskhanov11 <81189712+taimaskhanov11@users.noreply.github.com>
2022-03-02 01:11:48 +0300

    v 0.2 update

--- user_database_tg/app/filters/email_filter.py
+++ user_database_tg/app/filters/email_filter.py
@@ -11,7 +11,7 @@ class EmailFilter(BoundFilter):
         # data = ctx_data.get()
         # logger.info(f"4.Filter, {data=}")
         # if message.text[0].isalpha() and '@' in message.text:
-        match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', message.text)
+        match = re.match(r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', message.text.lower())
         # if "@" in message.text:
         logger.trace(match)
         if match:
