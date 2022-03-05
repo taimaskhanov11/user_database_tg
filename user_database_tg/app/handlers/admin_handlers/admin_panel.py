5916a6b3b73d043abd7d0cf6cead73aea5e77356
taimaskhanov11 <81189712+taimaskhanov11@users.noreply.github.com>
2022-03-05 11:35:16 +0300

    v 0.3.4.3 update

--- user_database_tg/app/handlers/admin_handlers/admin_panel.py
+++ user_database_tg/app/handlers/admin_handlers/admin_panel.py
@@ -2,11 +2,12 @@ from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 
 from user_database_tg.app.markups import admin_menu
+from user_database_tg.config.config import ADMINS
 from user_database_tg.db.models import DbUser
 
 
 async def admin_start(
-    message: types.Message, db_user: DbUser, state: FSMContext
+        message: types.Message, db_user: DbUser, state: FSMContext
 ):  # todo 2/27/2022 12:39 PM taima:
     await state.finish()
     await message.answer("Admin menu", reply_markup=admin_menu.admin_start)
@@ -29,12 +30,12 @@ def register_admin_menu_handlers(dp: Dispatcher):
         admin_start,
         commands="admin_start",
         # is_chat_admin=[1985947355, 2014301618]
-        user_id=[1985947355, 2014301618],
+        user_id=ADMINS,
         state="*",
     )
     dp.register_callback_query_handler(bot_settings, text="bot_settings")
     dp.register_message_handler(
-        admin_start,
+        no_admin_start,
         commands="admin_start",
         state="*",
     )
