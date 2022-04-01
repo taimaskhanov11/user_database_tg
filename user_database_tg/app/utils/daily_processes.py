e2117788cedd1b08f086fcedac81c843209f8b58
Rasul Taimaskhanov <81189712+taimaskhanov11@users.noreply.github.com>
2022-04-02 00:07:21 +0300

    fix everyday processes send message

--- user_database_tg/app/utils/daily_processes.py
+++ user_database_tg/app/utils/daily_processes.py
@@ -30,7 +30,6 @@ async def refresh_subscription():
                     logger.debug(f"Подписка закончилась {repr(sub.db_user)} ")
 
                     await bot.send_message(sub.db_user.user_id, f"Подписка {sub.title} закончилась")
-
                     sub.db_user.subscription = await Subscription.create(duration=datetime.datetime.now(TZ),
                                                                          )
                     await sub.db_user.save()
@@ -38,12 +37,10 @@ async def refresh_subscription():
                     continue
                 sub.remaining_daily_limit = sub.daily_limit if sub.is_subscribe else config.DAILY_LIMIT
                 await sub.save()
-                await bot.send_message(
-                    sub.db_user.user_id,
-                    f"Дневной лимит запросов обновлен.\n" f"У вас сейчас {sub.remaining_daily_limit}",
-                )
+                logger.success(
+                    f"{sub.db_user.username}[{sub.db_user.user_id}]|Дневной лимит запросов обновлен.({sub.remaining_daily_limit})")
             except Exception as e:
-                logger.critical(e)
+                logger.critical(f"Ошибка при обновлении лимитов {e}")
 
 
 @logger.catch
