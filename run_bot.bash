9d0ed20f2f8922c20eff2996cb53384645ada33f
Rasul Taimaskhanov <81189712+taimaskhanov11@users.noreply.github.com>
2022-05-08 17:42:32 +0300

    fix run bot fix

--- run_bot.bash
+++ run_bot.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-poetry run uvicorn api_server:app --host 0.0.0.0 --app-dir=user_database_tg/apps/api &
+poetry run uvicorn api_server:app --host 0.0.0.0 --app-dir=user_database_tg/api &
 poetry run python user_database_tg/main.py && fg
