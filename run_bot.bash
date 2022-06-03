74252ab1f5f34833a0b94274ba5e7e6d099157e2
Rasul Taimaskhanov <81189712+taimaskhanov11@users.noreply.github.com>
2022-06-03 23:59:13 +0300

    minor fix

--- run_bot.bash
+++ run_bot.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-killall gcalctool /home/user/.cache/pypoetry/virtualenvs/user-database-tg-hpGiraXy-py3.10/bin/python &&
-killall gcalctool ngrok &&
+killall gcalctool /home/user/.cache/pypoetry/virtualenvs/user-database-tg-hpGiraXy-py3.10/bin/python &
+killall gcalctool ngrok &
 poetry run uvicorn api_server:app --host 0.0.0.0 --app-dir=user_database_tg/api &
 poetry run python user_database_tg/main.py &
 ngrok http 8000 && fg
